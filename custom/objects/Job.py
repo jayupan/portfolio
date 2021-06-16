2e40ec1a539d6163be5a2aabf801efb774ada43f
jayupan <59853318+jayupan@users.noreply.github.com>
2021-06-16 02:39:14 -0500

    Create Job.py

---
+++ custom/objects/Job.py
@@ -0,0 +1,80 @@
+class Job:
+    def __init__(self, position="", salary = 0 , location="Chicago", url=None, distance=None):
+
+        if location == "Chicago":
+            self.salary = "$75,650"
+        elif location == "New_York":
+            self.salary = "$88,648"
+        elif location == "Los_Angeles":
+            self.salary = "$82,117"
+
+        self.position = position
+        self.location = location
+        # self.salary = salary
+
+        ## Experience Level Attributes
+        self.entryLevel = True
+        self.midLevel = True
+        self.seniorLevel = True
+
+
+        ## JobType status
+        self.fullTime = True 
+        self.partTime = False
+        self.contract = False
+        self.internship = False
+
+
+        self.url = url
+        self.distance = distance
+
+    #Modify Experience level Attributes
+    def flipEntryLevel(self):
+    	self.entryLevel = not self.entryLevel
+
+    def flipMidlevel(self):
+    	self.midLevel = not self.midLevel
+
+    def flipSeniorLevel(self):
+    	self.seniorLevel = not self.seniorLevel
+
+
+
+    #modify JobType Status
+    def flipFullTime(self):
+    	self.fullTime = not self.fullTime
+    
+    def flipPartTime(self):
+    	self.partTime = not self.partTime
+    
+    def flipContract(self):
+    	self.contract = not self.contract
+    
+    def flipInternship(self):
+    	self.internship = not self.internship
+
+    def isEntryLevel(self):
+        if 'associate' in self.position.lower() or 'analyst' in self.position.lower():
+            self.flipMidlevel()
+            self.flipSeniorLevel()
+
+    def isMidLevel(self):
+        if 'associate' in self.position.lower() or 'analyst' in self.position.lower():
+            self.flipEntryLevel()
+            self.flipSeniorLevel()
+
+    def isSrLevel(self):
+        if 'senior' in self.position.lower() or 'lead' in self.position.lower() or 'sr' in self.position.lower() or 'director' in self.position.lower():
+            self.flipMidlevel()
+            self.flipEntryLevel()
+
+    def isInternship(self):
+        if 'internship' in self.position.lower() or 'intern' in self.position.lower():
+            self.flipMidlevel()
+            self.flipSeniorLevel()
+            self.flipInternship()
+            self.flipFullTime()
+
+
+
+
